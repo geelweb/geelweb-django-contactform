fab18cd6186e2aec5adb0daeb4d4ded31bc5b402
Guillaume Luchet <guillaume@geelweb.org>
2014-11-25 22:56:24 +0100

    Add comments

--- src/geelweb/django/contactform/models.py
+++ src/geelweb/django/contactform/models.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# License: MIT
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+
+"""Django contactform models"""
+
 from django import forms
 
 class ContactForm(forms.Form):
+    """Contact form"""
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField()
     cc_myself = forms.BooleanField(required=False)
+
