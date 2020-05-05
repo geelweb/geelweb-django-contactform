3ba2d10207f3af025ac4dee50e1b67ff62b54925
Guillaume Luchet <gluchet@gmail.com>
2020-05-06 00:40:44 +0200

    update master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ project = 'django-contactform'
 copyright = '2020, Guillaume Luchet'
 author = 'Guillaume Luchet'
 
+master_doc = 'index'
+
 # The full version, including alpha/beta/rc tags
 release = '2.0.0'
 
