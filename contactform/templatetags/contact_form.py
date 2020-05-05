b445b6659e98d03401923ca06acfeece35a70258
Guillaume Luchet <gluchet@gmail.com>
2020-05-05 23:19:25 +0200

    simplify templatetag

--- contactform/templatetags/contact_form.py
+++ contactform/templatetags/contact_form.py
@@ -5,7 +5,7 @@ from contactform.forms import ContactForm
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
+@register.inclusion_tag('contactform/tags/contact_form.html', takes_context=True)
 def contact_form(context, form=None, *args, **kwargs):
     if form is None:
         next_url = kwargs['next'] if 'next' in kwargs else None
@@ -14,18 +14,9 @@ def contact_form(context, form=None, *args, **kwargs):
             })
 
     display_form_title = getattr(settings, 'CONTACTFORM_DISPLAY_FORM_TITLE', True)
-
-    template_name = 'contactform/form.html'
     framework = getattr(settings, 'CONTACTFORM_FRONTEND_FRAMEWORK', None)
-    if framework == 'bootstrap':
-        template_name = 'contactform/form_bootstrap.html'
 
-    if framework == 'uikit':
-        template_name = 'contactform/form_uikit.html'
-
-    t = context.template.engine.get_template(template_name)
-    return t.render(template.RequestContext(context.request, {
-        'form': form,
-        'display_form_title': display_form_title
-        }, autoescape=context.autoescape))
+    return {'form': form,
+            'display_form_title': display_form_title,
+            'framework': framework}
 
